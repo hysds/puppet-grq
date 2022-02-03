6cd068eae1f77f6846e9383bba481abefec9c221
Gerald Manipon <pymonger@gmail.com>
2022-02-03 15:36:06 -0800

    remove maria

--- manifests/mysqldb.pp
+++ manifests/mysqldb.pp
@@ -3,21 +3,21 @@ define grq::mysqldb ($user, $password, $admin_user, $admin_password, $sql_file="
     path => ["/bin", "/usr/bin"],
     unless => "mysql -u${admin_user} -p${admin_password} ${name}",
     command => "mysql -u${admin_user} -p${admin_password} -e \"create database ${name};\"",
-    require => Exec["mariadb-start"],
+    require => Exec["mysql-start"],
   }
 
   exec { "grant-${name}-db":
     path => ["/bin", "/usr/bin"],
     unless => "mysql -u${user} -p${password} ${name}",
     command => "mysql -u${admin_user} -p${admin_password} -e \"grant all on ${name}.* to ${user}@localhost identified by '$password';\"",
-    require => [Exec["mariadb-start"], Exec["create-${name}-db"]]
+    require => [Exec["mysql-start"], Exec["create-${name}-db"]]
   }
 
   if $sql_file != "" {
     exec { "import-${name}-db":
       path    => ["/bin", "/usr/bin"],
       command => "mysql -u${admin_user} -p${admin_password} ${name} < ${sql_file}",
-      require => [Exec["mariadb-start"], Exec["create-${name}-db"]]
+      require => [Exec["mysql-start"], Exec["create-${name}-db"]]
     }
   }
 }
