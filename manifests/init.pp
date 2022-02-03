cf644c32165acf42459d002abeffcd2f1ffd1256
Gerald Manipon <pymonger@gmail.com>
2022-02-03 14:59:13 -0800

    install mod_evasive from EL7 RPM

--- manifests/init.pp
+++ manifests/init.pp
@@ -77,7 +77,8 @@ class grq inherits hysds_base {
     'mailx': ensure => present;
     'httpd': ensure => present;
     'mod_ssl': ensure => present;
-    'mod_evasive': ensure => present;
+    #'mod_evasive': ensure => present;
+    'https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/m/mod_evasive-1.10.1-22.el7.x86_64.rpm': ensure => present;
     'geos-devel': ensure => installed;
     'proj-devel': ensure => installed;
     'geos-python': ensure => installed;
